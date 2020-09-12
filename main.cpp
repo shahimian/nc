695154db54f5eec9addccd2d527208bed182f8c5
Shahimian <shahimian@gmail.com>
2020-09-12 12:16:40 +0430

    sort of file

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 #include <stdio.h>
 #include "SimpsonMethod.h"
 
+void testSimpsonMethod();
+
 double f(double x){
 	return sqrt(x);
 }
 
 int main(int argc, char** argv) {
+	testSimpsonMethod();
+	return 0;
+}
+
+void testSimpsonMethod(){
 	SimpsonMethod *sm = new SimpsonMethod(f, 1, 1.3, 10);
 	printf("y=%.3f", sm->eval());
-	return 0;
+	return;
 }
 
